96d5f17ff8d460ad5da967376b0ff0f2d4c7175d
Madeline <116022163+MJGowan@users.noreply.github.com>
2022-10-20 16:31:18 -0700

    Add files via upload

--- GowanMMiniChallenge7/Program.cs
+++ GowanMMiniChallenge7/Program.cs
@@ -1,13 +1,16 @@
 ﻿//Maddie Gowan
-//09-16-2022
-//Mini Challenge #7 - Reverse It
-//
-//Peer reviewed by: Kenneth Fujimura
-//I really liked how your program was formatted, to both acknowledge
-//when a number was given and something not a number. You could have probably
-//further simplified your code by making the for loop its own function that
-//you could call like they demo’d to us in yesterday’s lecture, but as is -
-//everything runs perfectly! Great job!
+//10-20-2022
+//Mini Challenge #7 - Redo
+//In this program the user input must be reversed. It should reverse whether it's
+//a string or a number.
+
+//Peer Review: Brandon Le - overall the program did what it was meant to do and it
+//reversed the strings and numbers. Play again function worked as intended I was
+//able to hit yes to play again and any other typing, no or anything other than yes
+//will end the program. Good job! If there was one small thing to add I would do
+//data validation for the "If you would like to play again" at the end. So if user
+//accidentally types ys ye or some other typo instead of yes, it won't end the program
+//and it would allow them to properly input yes. But very good job :)
 
 Console.Clear();
 
